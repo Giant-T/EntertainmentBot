806bddc6577b3739da862e8f262c52f707d05d6b
William Boudreault <boudreaultwilliam@gmail.com>
2023-05-13 00:04:37 -0400

    Ajout .env

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+import * as dotenv from 'dotenv';
+
 async function main(): Promise<void> {
   console.log('Démarrage en cours...');
+  dotenv.config();
+
+  console.log(process.env.TOKEN);
+
   console.log('Fin du programme.');
 }
 
